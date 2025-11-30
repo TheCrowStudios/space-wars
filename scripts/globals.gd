72d2ff95147118610ed64e5923d6fe6de6031e99
TheCrowStudios <madgamersmemes@gmail.com>
2025-11-30 18:53:03 +0000

    create fuel tank, fuel tank armor, and oxygen tank as separate objects

--- scripts/globals.gd
+++ scripts/globals.gd
@@ -11,4 +11,4 @@ enum BulletType {
 }
 
 const bulletPenetrations = {BulletType.LIGHT: 4, BulletType.MEDIUM: 8, BulletType.HEAVY: 15, BulletType.PELLET: 1, BulletType.SLUG: 30, BulletType.POWER_BEAM: 100}
-const DEBUG: bool = true
+const DEBUG: bool = false
