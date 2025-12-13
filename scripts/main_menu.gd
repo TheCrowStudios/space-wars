ca51c547bbca9c44a42b7ca856e2a75500aafdbd
TheCrowStudios <madgamersmemes@gmail.com>
2025-12-13 15:38:01 +0000

    add main menu

---
+++ scripts/main_menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	# $WorldEnvironment.environment.glow_hdr_threshold += randf_range(-0.5, 0.5)
+	# $WorldEnvironment.environment.glow_hdr_threshold = max($WorldEnvironment.environment.glow_hdr_threshold, 0.6)
+	# $WorldEnvironment.environment.glow_hdr_threshold = min($WorldEnvironment.environment.glow_hdr_threshold, 0.8)
+	pass
