2abae64f276435eee06dcba5d9f19b1d98d13cf8
TheCrowStudios <madgamersmemes@gmail.com>
2025-12-19 00:57:42 +0000

    separate camera controls into own scene

---
+++ scripts/camera_controller.gd
@@ -0,0 +1,73 @@
+extends Node2D
+
+@export var max_zoom: float = 2.0
+@export var min_zoom: float = 0.5
+@export var max_shift: int = 400
+
+var target_zoom: float = 1.0
+var target_shift: Vector2 = Vector2.ZERO
+var move_camera_to_mouse: bool = false
+var camera: Camera2D
+
+func _ready() -> void:
+	camera = get_viewport().get_camera_2d()
+	print(camera.global_position)
+	print(camera.position)
+	camera.position.x += 10
+	print(camera.global_position)
+	print(camera.position)
+
+func _process(delta: float) -> void:
+	camera.zoom.x = lerp(camera.zoom.x, target_zoom, 0.1)
+	camera.zoom.y = camera.zoom.x
+	camera.global_rotation_degrees = 0
+	camera.rotation_degrees = 0
+
+	if move_camera_to_mouse:
+		target_shift = get_global_mouse_position() - camera.global_position
+		target_shift = target_shift.clamp(Vector2(-max_shift, -max_shift), Vector2(max_shift, max_shift))
+	else:
+		target_shift = Vector2.ZERO
+
+	camera.position = lerp(camera.position, target_shift, 0.1)
+
+	var zoom_factor = camera.zoom
+	var inverse_zoom = Vector2(1.0 / zoom_factor.x, 1.0 / zoom_factor.y)
+
+	var volume = linear_to_db(camera.zoom.x) * 1.5
+
+	# volume = min(volume, 0.0)
+	volume = clamp(volume, -24.0, 0.0)
+	# print(volume)
+
+	AudioServer.set_bus_volume_db(0, volume)
+
+func _input(event: InputEvent) -> void:
+	var zoom = false
+
+	if event.is_action_pressed("mouse_wheel_up"):
+		zoom = true
+		if target_zoom < max_zoom:
+			target_zoom += 0.1
+			target_zoom = min(target_zoom, max_zoom)
+	
+	if event.is_action_pressed("mouse_wheel_down"):
+		zoom = true
+		if target_zoom > min_zoom:
+			target_zoom -= 0.1
+			target_zoom = max(target_zoom, min_zoom)
+	
+	if event.is_action_pressed("shift"):
+		move_camera_to_mouse = true
+
+	if event.is_action_released("shift"):
+		move_camera_to_mouse = false
+
+func get_zoom() -> Vector2:
+	return camera.zoom
+
+func set_target_zoom(zoom: float):
+	target_zoom = zoom
+
+func set_target_shift(shift: Vector2):
+	target_shift = shift
--- scripts/node_2d.gd
+++ scripts/node_2d.gd
@@ -1,32 +1,17 @@
 extends Node2D
 
 
-@export var max_zoom: float = 2.0
-@export var min_zoom: float = 0.5
-@export var max_shift: int = 400
-
 @onready var map_canvas: CanvasLayer = get_node("MapCanvas")
 @onready var map_icons: Node2D = map_canvas.get_node("MapIcons")
 
 const SPACESHIP: PackedScene = preload("res://scenes/spaceship.tscn")
 const MAP_ENEMY_DOT: PackedScene = preload("res://scenes/map_enemy_dot.tscn")
 
-var target_zoom: float = 1.0
-var target_shift: Vector2 = Vector2.ZERO
-var move_camera_to_mouse: bool = false
-var camera: Camera2D
 var on_map: Array
-var map_update = false
 
 var spaceships_minimap: Dictionary = {}
 
 func _ready():
-	camera = get_viewport().get_camera_2d()
-	print(camera.global_position)
-	print(camera.position)
-	camera.position.x += 10
-	print(camera.global_position)
-	print(camera.position)
 	# for child in get_children():
 	# 	if child.is_in_group("OnMap"):
 	# 		on_map.append(child)
@@ -34,16 +19,10 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	camera.zoom.x = lerp(camera.zoom.x, target_zoom, 0.1)
-	camera.zoom.y = camera.zoom.x
-	camera.global_rotation_degrees = 0
-	camera.rotation_degrees = 0
-
-	if camera.zoom.x <= 0.2:
+	if %CameraController.get_zoom().x <= 0.2:
 		map_canvas.show()
 		get_viewport().set_canvas_cull_mask_bit(0, false)
 		get_viewport().set_canvas_cull_mask_bit(1, true)
-		map_update = false
 
 		for node: Node2D in on_map:
 			if is_instance_valid(node):
@@ -73,16 +52,6 @@ func _process(delta: float) -> void:
 				# node.map_icon_instance.hide()
 				pass
 
-	if move_camera_to_mouse:
-		target_shift = get_global_mouse_position() - camera.global_position
-		target_shift = target_shift.clamp(Vector2(-max_shift, -max_shift), Vector2(max_shift, max_shift))
-	else:
-		target_shift = Vector2.ZERO
-
-	camera.position = lerp(camera.position, target_shift, 0.1)
-
-	var zoom_factor = camera.zoom
-	var inverse_zoom = Vector2(1.0 / zoom_factor.x, 1.0 / zoom_factor.y)
 	# print(camera.zoom)
 
 	# for child in get_children():
@@ -93,41 +62,12 @@ func _process(delta: float) -> void:
 
 	# $ParallaxBackground.scale = inverse_zoom
 
-	var volume = linear_to_db(camera.zoom.x) * 1.5
-
-	# volume = min(volume, 0.0)
-	volume = clamp(volume, -24.0, 0.0)
-	# print(volume)
-
-	AudioServer.set_bus_volume_db(0, volume)
-
 	update_minimap()
 
 func register_map_icon(node):
 	on_map.append(node)
 
 func _input(event: InputEvent) -> void:
-	var zoom = false
-
-	if event.is_action_pressed("mouse_wheel_up"):
-		zoom = true
-		if target_zoom < max_zoom:
-			target_zoom += 0.1
-			target_zoom = min(target_zoom, max_zoom)
-	
-	if event.is_action_pressed("mouse_wheel_down"):
-		zoom = true
-		if target_zoom > min_zoom:
-			target_zoom -= 0.1
-			target_zoom = max(target_zoom, min_zoom)
-			map_update = true
-	
-	if event.is_action_pressed("shift"):
-		move_camera_to_mouse = true
-
-	if event.is_action_released("shift"):
-		move_camera_to_mouse = false
-
 	if event.is_action_pressed("ui_home"):
 		spawn_enemy()
 	
@@ -165,8 +105,8 @@ func update_minimap():
 		# print("UPDATING SPACESHIP")
 
 func _on_player_ship_character_died(node: Spaceship) -> void:
-	target_zoom = 2.0
-	target_shift = Vector2.ZERO
+	%CameraController.set_target_zoom(2.0)
+	%CameraController.set_target_shift(Vector2.ZERO)
 
 func _on_ai_spaceship_character_died(node: Spaceship) -> void:
 	spaceships_minimap[node].queue_free()
